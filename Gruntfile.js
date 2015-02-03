7d09fb1fdf58fcc7bc06da49a04085cf9c460f7f
MichalZalecki <zalecki.post@gmail.com>
2015-02-04 00:39:48 +0100

    Init

---
+++ Gruntfile.js
@@ -0,0 +1,52 @@
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        traceur: {
+            options: {
+                experimental: true,
+                moduleNames: false
+            },
+            custom: {
+                files: [{
+                    expand: true,
+                    cwd: 'src/es6',
+                    src: ['*.js'],
+                    dest: 'src/es5'
+                }]
+            }
+        },
+        karma: {
+            options: {
+                configFile: 'karma.conf.js'
+            },
+            watch: {
+                autoWatch: false,
+                background: true,
+                singleRun: false
+            },
+            single: {
+                singleRun: true
+            }
+        },
+        watch: {
+            options: {
+                interrupt: true
+            },
+            es6: {
+                files: ['src/es6/*.js'],
+                tasks: ['traceur', 'karma:watch:run']
+            }
+        }
+    });
+
+    // Load plugins.
+    grunt.loadNpmTasks('grunt-traceur');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    // Default task(s).
+    grunt.registerTask('default', ['traceur', 'karma:single']);
+
+};
---
+++ README.md
@@ -0,0 +1,8 @@
+# ES6 Features Runtime
+
+It helps you to start shaping up whit ECMAScript 6 using [Traceur]
+(https://github.com/google/traceur-compiler) compiler. And what is the best
+way to start learning new stuff? Unit testing! That's why ES6 Features
+Runtime is shipped with Karma and Jasmine.
+
+Write in ES6 today and compile it to ES5!
