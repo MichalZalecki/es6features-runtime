f9cd7467437f24deb9a24490a176a19fd979554b
MichalZalecki <zalecki.post@gmail.com>
2015-02-05 19:35:46 +0100

    Enhanced Object Literals comments improved

--- src/es6/enhanced-object-literals.js
+++ src/es6/enhanced-object-literals.js
@@ -5,17 +5,16 @@ describe("Enhanced Object Literals", () => {
             return "Hello " + name;
         }
         var obj = {
-            // Computed (dynamic) property names
+            // Computed property names
             [x*2]: "Computed Property Name",
             // __proto__
             __proto__: {
                 hi: function () { return "Hi!" },
                 by: function () { return "By!" }
             },
-            // Shorthand for ‘handler: handler’
+            // Shorthand for greet: greet
             greet
         };
-        // Computed (dynamic) property names
         expect(obj[4]).toEqual("Computed Property Name");
         expect(obj.hi()).toEqual("Hi!");
         expect(obj.by()).toEqual("By!");
