0be10fa7c2d5d5f762bf2389d710e442c62eca21
MichalZalecki <zalecki.post@gmail.com>
2015-02-14 00:34:48 +0100

    Numeric Literals

---
+++ src/es5/numeric-literals.js
@@ -0,0 +1,13 @@
+$traceurRuntime.ModuleStore.getAnonymousModule(function() {
+  "use strict";
+  describe("Numeric Literals", (function() {
+    it("should handle binary (2)", (function() {
+      expect([7, 240, 15]).toEqual([7, 240, 15]);
+    }));
+    it("should handle octal (8)", (function() {
+      expect([7, 240, 15]).toEqual([7, 240, 15]);
+    }));
+  }));
+  return {};
+});
+//# sourceURL=src/es6/numeric-literals.js
